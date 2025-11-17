42ec3758749e656d57ee678df6bd59925d39a86b
AramSarkissian <as3325@cam.ac.uk>
2025-11-17 14:46:26 +0000

    a

--- MHrank.py
+++ MHrank.py
@@ -20,7 +20,9 @@ def MH_sample(games, num_players, num_its):
 
     w = np.zeros(num_players)  # skill for each player
 
-    sigma_prop = 0.5  # proposal std dev
+    sigma_prop = 0.1  # proposal std dev
+    w_mean = []
+    w_std = []
 
     for itr in tqdm(range(num_its)):
         for i in range(num_players):
@@ -39,6 +41,10 @@ def MH_sample(games, num_players, num_its):
                 w[i] = w_prop
 
         skill_samples[:, itr] = w
+        w_mean.append(np.mean(w))
+        w_std.append(np.std(w))
 
-    return skill_samples
+
+    return skill_samples, w_mean, w_std
+
 
